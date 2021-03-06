adb439d5aba8cd43b7334a15b1075d5d7ca2cbc1
TheCoder11 <jtsjohn05@gmail.com>
2021-03-06 13:03:19 -0800

    AB2, Save/Load

---
+++ src/main/java/com/somemone/bigventories/command/AdminCommand.java
@@ -0,0 +1,15 @@
+package com.somemone.bigventories.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class AdminCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+
+
+        return true;
+    }
+}
